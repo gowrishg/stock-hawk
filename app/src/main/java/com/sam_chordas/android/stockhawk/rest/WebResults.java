d1212ec354389990d1d91a9ba536b9413ae4351b
gowrishg-mac-bookpro <gowrishg@qi.sg>
2016-04-25 00:15:03 +0800

    Adds the 'Response' codes for the network (alternate for enum)

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/rest/WebResults.java
@@ -0,0 +1,23 @@
+package com.sam_chordas.android.stockhawk.rest;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by gowrishg on 24/4/16.
+ */
+public class WebResults {
+
+    @IntDef({STATUS_OK, STATUS_SERVER_DOWN, STATUS_SERVER_INVALID, STATUS_UNKNOWN, STATUS_NO_INTERNET})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Response {
+    }
+
+    public static final int STATUS_OK = 0;
+    public static final int STATUS_SERVER_DOWN = 1;
+    public static final int STATUS_SERVER_INVALID = 2;
+    public static final int STATUS_UNKNOWN = 3;
+    public static final int STATUS_NO_INTERNET = 4;
+}
